470f3caccf05892bf00d41ac037c1f8f083db809
Sean Dougherty <sean.dougherty.cs@gmail.com>
2014-09-29 11:50:05 -0400

    2-bit experiment

--- src/experiments/sequence.cpp
+++ src/experiments/sequence.cpp
@@ -341,3 +341,151 @@ public:
         };
     }
 } seq_abc;
+
+class Sequential2bitExperiment : public Experiment {
+public:
+    Sequential2bitExperiment()
+        : Experiment("seq-2bit") {
+    }
+
+    virtual void init_env() override {
+        ::init_env();
+    }
+
+    virtual vector<Test> create_tests() override {
+#define _A 0.0, 0.0
+#define _B 0.0, 1.0
+#define _C 1.0, 0.0
+#define _D 1.0, 1.0
+#define __ 0.0, 0.0
+
+        const float S = 1.0; // Signal
+        const float Q = 1.0; // Query
+        const float _ = 0.0; // Null
+
+        const real_t weight_seq = 4;
+        const real_t weight_delay = 25;
+        const real_t weight_query = 55;
+
+        return {
+            {{
+                    {{S, _, _A}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _A}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_A, _A}, weight_query}
+            }},                        
+            {{                         
+                    {{S, _, _A}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _B}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_A, _B}, weight_query}
+            }},                        
+            {{                         
+                    {{S, _, _A}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _C}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_A, _C}, weight_query}
+            }},                        
+            {{                         
+                    {{S, _, _A}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _D}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_A, _D}, weight_query}
+            }},                        
+            {{                         
+                    {{S, _, _B}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _A}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_B, _A}, weight_query}
+            }},                        
+            {{                         
+                    {{S, _, _B}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _B}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_B, _B}, weight_query}
+            }},                        
+            {{                         
+                    {{S, _, _B}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _C}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_B, _C}, weight_query}
+            }},                        
+            {{                         
+                    {{S, _, _B}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _D}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_B, _D}, weight_query}
+            }},                        
+            {{                         
+                    {{S, _, _C}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _A}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_C, _A}, weight_query}
+            }},                        
+            {{                         
+                    {{S, _, _C}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _B}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_C, _B}, weight_query}
+            }},                        
+            {{                         
+                    {{S, _, _C}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _C}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_C, _C}, weight_query}
+            }},                        
+            {{                         
+                    {{S, _, _C}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _D}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_C, _D}, weight_query}
+            }},                        
+            {{                         
+                    {{S, _, _D}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _A}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_D, _A}, weight_query}
+            }},                        
+            {{                         
+                    {{S, _, _D}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _B}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_D, _B}, weight_query}
+            }},                        
+            {{                         
+                    {{S, _, _D}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _C}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_D, _C}, weight_query}
+            }},                        
+            {{                         
+                    {{S, _, _D}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_seq},
+                    {{S, _, _D}, {__, __}, weight_seq},
+                    {{_, _, __}, {__, __}, weight_delay},
+                    {{_, Q, __}, {_D, _D}, weight_query}
+            }}
+        };
+
+#undef _A
+#undef _B
+#undef _C
+#undef _D
+#undef __
+    }
+} seq_2bit;
