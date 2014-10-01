907cd829215ef0cfde6073954db58050276c861f
Sean Dougherty <sean.dougherty.cs@gmail.com>
2014-10-01 10:39:33 -0400

    Use higher compat_threshold

--- src/experiments/xor.cpp
+++ src/experiments/xor.cpp
@@ -10,7 +10,7 @@ public:
     }
 
     virtual void init_env() override {
-        // use legacy defaults
+        compat_threshold = 10.0;
     }
 
     virtual vector<Test> create_tests() override {
