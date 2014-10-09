48a4f89bddee5fae4e7fc25fcb46461ad18d4595
Sean Dougherty <sean.dougherty.cs@gmail.com>
2014-10-09 16:05:43 -0400

    sequential or parallel evaluation

--- src/organismevaluator.cpp
+++ src/organismevaluator.cpp
@@ -23,13 +23,9 @@ bool OrganismEvaluator::evaluate(std::function<bool (Organism &org, size_t istep
     static Timer timer("evaluate");
     timer.start();
 
-    size_t nthreads = omp_get_max_threads();
-    Organism *fittest_thread[nthreads];
-
-    for(size_t i = 0; i < nthreads; i++)
-        fittest_thread[i] = nullptr;
-
     size_t norgs = pop->size();
+
+#if true
 #pragma omp parallel for
     for(size_t i = 0; i < norgs; i++) {
         Organism *org = pop->get(i);
@@ -42,30 +38,38 @@ bool OrganismEvaluator::evaluate(std::function<bool (Organism &org, size_t istep
         }
 
         org->eval = eval(*org);
-
-        size_t tnum = omp_get_thread_num();
-        if( (fittest_thread[tnum] == nullptr)
-            || (org->eval.fitness > fittest_thread[tnum]->eval.fitness) ) {
-
-            fittest_thread[tnum] = org;
+    }
+#else
+    bool remaining = true;
+    for(size_t istep = 0; remaining; istep++) {
+        remaining = false;
+#pragma omp parallel for reduction(||:remaining)
+        for(size_t iorg = 0; iorg < norgs; iorg++) {
+            Organism *org = pop->get(iorg);
+            if(prepare_step(*org, istep)) {
+                remaining = true;
+                for(size_t j = 0; j < NACTIVATES_PER_INPUT; j++) {
+                    org->net.activate();
+                }
+                eval_step(*org, istep);
+            } else {
+                org->eval = eval(*org);
+            }
         }
     }
+#endif
 
-    Organism *best = nullptr;
-    for(size_t i = 0; i < nthreads; i++) {
-        if( !best
-            || (fittest_thread[i]
-                && (fittest_thread[i]->eval.fitness > best->eval.fitness)) ) {
-
-            best = fittest_thread[i];
+    Organism *best = pop->get(0);
+    for(size_t i = 1; i < norgs; i++) {
+        Organism *org = pop->get(i);
+        if(org->eval.fitness > best->eval.fitness) {
+            best = org;
         }
     }
 
     timer.stop();
 
-    if( best &&
-       (!fittest || (best->eval.fitness > fittest->eval.fitness)) ) {
-
+    if(!fittest || (best->eval.fitness > fittest->eval.fitness)) {
         fittest = pop->make_copy(best->population_index);
         return true;
     } else {
